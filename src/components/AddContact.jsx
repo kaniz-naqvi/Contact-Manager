15122d89de832c914aed1a88bffc1a33c8749e34
Mehak <mehak313naqvi@gmail.com>
2025-01-04 12:46:16 +0500

    Added popup functionality to add contact

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import FeildSets from "./FeildSets";
 import { Button } from "./Icons";
+import { Link } from "react-router-dom";
 
 const AddContact = ({ addContact }) => {
   const [contactName, setContactName] = useState("");
   const [contactEmail, setContactEmail] = useState("");
   const [contactNumber, setContactNumber] = useState("");
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   function handelSubmit(e) {
     e.preventDefault();
@@ -18,44 +20,62 @@ const AddContact = ({ addContact }) => {
       };
 
       addContact(newContact); // Pass contact to App.js
-
+      setIsPopupVisible(true);
       // Reset input fields
       setContactName("");
       setContactEmail("");
       setContactNumber("");
+      // Hide the popup after 3 seconds
+      setTimeout(() => {
+        setIsPopupVisible(false);
+      }, 2500);
     } else {
-      alert("Each Feild is required!");
+      alert("Each field is required!");
     }
   }
 
   return (
-    <form className="container border rounded p-lg-4 p-sm-1 py-4 w-75 mt-4">
-      <h2 className="text-center fs-4">Add Contact</h2>
-      <FeildSets
-        value={contactName}
-        setValue={setContactName}
-        name={"Name"}
-        placeholder={"Enter Contact Name"}
-      />
-      <FeildSets
-        value={contactEmail}
-        setValue={setContactEmail}
-        name={"Email"}
-        placeholder={"Enter Contact Email"}
-      />
-      <FeildSets
-        value={contactNumber}
-        setValue={setContactNumber}
-        name={"Number"}
-        placeholder={"Enter Contact's phone Number"}
-      />
-      <Button
-        text="Add"
-        icon="bi-plus-lg"
-        color={"primary mt-2"}
-        onClick={handelSubmit}
-      />
-    </form>
+    <>
+      <form className="container border rounded p-lg-4 p-sm-1 py-4 w-75 mt-4">
+        {isPopupVisible && (
+          <div className="d-flex justify-content-center align-items-center">
+            <Button
+              icon={"bi-check-circle-fill"}
+              color={"success"}
+              text={`Contact Saved.`}
+              link={true}
+              linkTo={"/"}
+              linkText={" View Here"}
+            />
+          </div>
+        )}
+        <h2 className="text-center fs-4">Add Contact</h2>
+        <FeildSets
+          value={contactName}
+          setValue={setContactName}
+          name={"Name"}
+          placeholder={"Enter Contact Name"}
+        />
+        <FeildSets
+          value={contactEmail}
+          setValue={setContactEmail}
+          name={"Email"}
+          placeholder={"Enter Contact Email"}
+        />
+        <FeildSets
+          value={contactNumber}
+          setValue={setContactNumber}
+          name={"Number"}
+          placeholder={"Enter Contact's phone Number"}
+        />
+        <Button
+          text="Add"
+          icon="bi-plus-lg"
+          color={"primary mt-2"}
+          onClick={handelSubmit}
+        />
+      </form>
+    </>
   );
 };
 
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const Button = ({ text, icon, color, textColor, onClick }) => {
+export const Button = ({
+  text,
+  icon,
+  color,
+  textColor,
+  onClick,
+  link,
+  linkText,
+  linkTo,
+}) => {
   return (
     <button
       type="button"
@@ -9,6 +19,11 @@ export const Button = ({ text, icon, color, textColor, onClick }) => {
     >
       {icon && <i className={`bi ${icon} m-1`}></i>}
       {text && <span>{text}</span>}
+      {link && (
+        <Link className="text-light" to={linkTo}>
+          {linkText}
+        </Link>
+      )}
     </button>
   );
 };
