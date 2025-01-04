d18ec68bc36386e39a0cb356fe0b0fd9e7b9326c
Mehak <mehak313naqvi@gmail.com>
2025-01-04 12:19:10 +0500

    Add search functionality to contact manager

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./Icons";
 import ContactCard from "./ContactCard";
 import { Link } from "react-router-dom";
 
 const ContactList = ({ contacts, deleteContact }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [filteredContacts, setFilteredContacts] = useState(contacts);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const filteredContact = contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(inputValue.toLowerCase());
+    });
+    setFilteredContacts(filteredContact);
+  };
   return (
     <div className="container w-75 pt-4">
       <div className="d-flex  justify-content-between align-items-center">
@@ -12,18 +21,23 @@ const ContactList = ({ contacts, deleteContact }) => {
           <Button icon="bi-plus-lg" color={"primary rounded-circle"} />
         </Link>
       </div>
-      <form className="d-flex gap-0">
+      <form onSubmit={(e) => handelSubmit(e)} className="d-flex gap-0">
         <input
+          value={inputValue}
           type="text"
           placeholder="Search Contacts"
           className="rounded-pill my-2 w-100 px-3 py-1 text-light"
+          onChange={(e) => setInputValue(e.target.value)}
         />
-        <div className="search-btn px-3 mt-2 bg-light text-dark">
+        <button
+          type="submit"
+          className="btn rounded-0 search-btn px-3 mt-2 bg-light text-dark"
+        >
           <i className="bi bi-search"></i>
-        </div>
+        </button>
       </form>
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <ContactCard
           key={contact.id}
           deleteContact={deleteContact}
